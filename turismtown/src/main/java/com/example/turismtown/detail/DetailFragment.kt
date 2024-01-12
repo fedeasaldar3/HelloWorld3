f3b6203faf10d9b01a942c3bc3ba1b3e1e45dfa4
fedeasaldar3 <fedeasaldar3@gmail.com>
2024-01-12 11:13:21 -0500

    *App with MVVM and LiveData

--- turismtown/src/main/java/com/example/turismtown/detail/DetailFragment.kt
+++ turismtown/src/main/java/com/example/turismtown/detail/DetailFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import com.example.turismtown.databinding.FragmentDetailBinding
 import com.example.turismtown.main.MainActivity
@@ -13,6 +15,7 @@ import com.squareup.picasso.Picasso
 class DetailFragment : Fragment() {
 
     private lateinit var detailBinding: FragmentDetailBinding
+    private val detailViewModel : DetailViewModel by viewModels()
     private val args: DetailFragmentArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- turismtown/src/main/java/com/example/turismtown/list/ListFragment.kt
+++ turismtown/src/main/java/com/example/turismtown/list/ListFragment.kt
@@ -5,34 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.turismtown.databinding.FragmentListBinding
 import com.example.turismtown.main.MainActivity
-import com.example.turismtown.model.Place
 import com.example.turismtown.model.PlaceItem
-import com.google.gson.Gson
 
 
 class ListFragment : Fragment() {
 
     private lateinit var listBinding: FragmentListBinding
+    private lateinit var listViewModel: ListViewModel
     private lateinit var placesAdapter: PlacesAdapter
-    private lateinit var listPlaces: ArrayList<PlaceItem>
+    private var listPlaces: ArrayList<PlaceItem> = arrayListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         listBinding = FragmentListBinding.inflate(inflater, container, false)
+        listViewModel = ViewModelProvider(this)[ListViewModel::class.java]
         return listBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (activity as MainActivity).hideIcon()
-        listPlaces = loadMockPlaceFromJson()
+
+        listViewModel.loadMockPlaceFromJson(context?.assets?.open("places_json.json"))
+
+        listViewModel.onPlacesLoaded.observe(viewLifecycleOwner) {
+            onPlacesLoadedSubscribe(it)
+        }
+
+    }
+
+    private fun onPlacesLoadedSubscribe(listPlaces: ArrayList<PlaceItem>) {
         placesAdapter = PlacesAdapter(onItemClicked = { onPlaceClicked(it) }, listPlaces)
+
         listBinding.placesRv.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = placesAdapter
@@ -44,12 +55,5 @@ class ListFragment : Fragment() {
         findNavController().navigate(ListFragmentDirections.actionListFragmentToDetailFragment(place))
     }
 
-    private fun loadMockPlaceFromJson(): ArrayList<PlaceItem> {
-        val placesString: String =
-            context?.assets?.open("places_json.json")?.bufferedReader().use { it!!.readText() }
 
-        val gson = Gson()
-        val data = gson.fromJson(placesString, Place::class.java)
-        return data
-    }
 }
